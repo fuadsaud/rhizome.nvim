519d7c7eb6100d5c5b9909e762f6c4fa5aab3665
Fuad Saud <fuadfsaud@gmail.com>
2022-12-29 17:08:03 +0100

    Add label function

--- fnl/rhizome.fnl
+++ fnl/rhizome.fnl
@@ -1,5 +1,6 @@
 (module rhizome
   {autoload {a aniseed.core
+             string aniseed.string
              pickers telescope.pickers
              finders telescope.finders
              actions telescope.actions
@@ -7,10 +8,16 @@
              config telescope.config}})
 
 (local *config* {:roots []
+                 :label_fn (fn [root]
+                             (print root)
+                             (a.last (string.split (. root :path) "/")))
                  :telescope_options {}})
 
 (defn known_roots []
   (. *config* :roots))
+
+(fn default_label [root]
+  ((. *config* :label_fn) root))
 
 (fn normalize_path
   [path]
@@ -27,10 +34,14 @@
   [known_roots input_path]
   (a.first (a.filter (fn [root] (path_match? root.path input_path)) known_roots)))
 
-(fn derive_root
+(fn derive_root*
   [known_roots input_path]
   (or (find_in_known_roots known_roots input_path)
       {:path input_path}))
+
+(fn derive_root
+  [input_path]
+  (derive_root* (. *config* :roots) input_path))
 
 (fn open
   [cmd cwd entrypoint]
@@ -39,29 +50,37 @@
 
 (fn open_root
   [cmd root]
-  (open cmd (. root :path) (or (a.str (. root :path) "/" (. root :entrypoint))
+  (open cmd (. root :path) (or (a.join "/" [(. root :path) (. root :entrypoint)])
                                (. root :path))))
+
+(defn label* [input_path]
+  (let [root (derive_root input_path)]
+    (or (. root :label)
+        (default_label root))))
+
+(defn label [tabnr]
+  (label* (vim.fn.getcwd -1 tabnr)))
 
 (defn open_in_current_tab
   [input_path]
-  (open_root :edit (derive_root (. *config* :roots) input_path)))
+  (open_root :edit (derive_root input_path)))
 
 (defn open_in_new_tab
   [input_path]
-  (open_root :tabedit (derive_root (. *config* :roots) input_path)))
+  (open_root :tabedit (derive_root input_path)))
 
 (defn setup
-  [{:roots roots
-    :telescope_options telescope_options}]
-  (tset *config* :roots roots)
-  (tset *config* :telescope_options telescope_options))
+  [opts]
+  (a.merge! *config* opts))
 
 (comment
-  (local zsh_path "/Users/fuad/.config/zsh")
-  (derive_root (. *config* :roots) zsh_path)
   (setup {:roots [{:path "~/Source/fuadsaud/Monrovia"}
                   {:path "~/Source/fuadsaud/fuadsaud.github.io"}
                   {:path "~/.ditmas"}
                   {:path "~/.config/nvim"}
                   {:path "~/.config/zsh" :entrypoint ".zshrc"}]})
+  (. *config* :roots)
+  (local zsh_path "/Users/fuad/.config/zsh")
+  (local zsh_root (derive_root zsh_path))
+  (label* zsh_path)
   (open_in_new_tab zsh_path))
--- lua/rhizome.lua
+++ lua/rhizome.lua
@@ -11,18 +11,27 @@ do
   _2amodule_locals_2a = (_2amodule_2a)["aniseed/locals"]
 end
 local autoload = (require("aniseed.autoload")).autoload
-local a, action_state, actions, config, finders, pickers = autoload("aniseed.core"), autoload("telescope.actions.state"), autoload("telescope.actions"), autoload("telescope.config"), autoload("telescope.finders"), autoload("telescope.pickers")
+local a, action_state, actions, config, finders, pickers, string = autoload("aniseed.core"), autoload("telescope.actions.state"), autoload("telescope.actions"), autoload("telescope.config"), autoload("telescope.finders"), autoload("telescope.pickers"), autoload("aniseed.string")
 do end (_2amodule_locals_2a)["a"] = a
 _2amodule_locals_2a["action_state"] = action_state
 _2amodule_locals_2a["actions"] = actions
 _2amodule_locals_2a["config"] = config
 _2amodule_locals_2a["finders"] = finders
 _2amodule_locals_2a["pickers"] = pickers
-local _2aconfig_2a = {roots = {}, telescope_options = {}}
+_2amodule_locals_2a["string"] = string
+local _2aconfig_2a
+local function _1_(root)
+  print(root)
+  return a.last(string.split(root.path, "/"))
+end
+_2aconfig_2a = {roots = {}, label_fn = _1_, telescope_options = {}}
 local function known_roots()
   return (_2aconfig_2a).roots
 end
 _2amodule_2a["known_roots"] = known_roots
+local function default_label(root)
+  return (_2aconfig_2a).label_fn(root)
+end
 local function normalize_path(path)
   return vim.fn.resolve(vim.fn.expand(path))
 end
@@ -30,41 +39,48 @@ local function path_match_3f(path_a, path_b)
   return (normalize_path(path_a) == normalize_path(path_b))
 end
 local function find_in_known_roots(known_roots0, input_path)
-  local function _1_(root)
+  local function _2_(root)
     return path_match_3f(root.path, input_path)
   end
-  return a.first(a.filter(_1_, known_roots0))
+  return a.first(a.filter(_2_, known_roots0))
 end
-local function derive_root(known_roots0, input_path)
+local function derive_root_2a(known_roots0, input_path)
   return (find_in_known_roots(known_roots0, input_path) or {path = input_path})
+end
+local function derive_root(input_path)
+  return derive_root_2a((_2aconfig_2a).roots, input_path)
 end
 local function open(cmd, cwd, entrypoint)
   vim.cmd({cmd = cmd, args = {entrypoint}})
   return vim.cmd({cmd = "tcd", args = {cwd}})
 end
 local function open_root(cmd, root)
-  return open(cmd, root.path, (a.str(root.path, "/", root.entrypoint) or root.path))
+  return open(cmd, root.path, (a.join("/", {root.path, root.entrypoint}) or root.path))
 end
+local function label_2a(input_path)
+  local root = derive_root(input_path)
+  return (root.label or default_label(root))
+end
+_2amodule_2a["label*"] = label_2a
+local function label(tabnr)
+  return label_2a(vim.fn.getcwd(-1, tabnr))
+end
+_2amodule_2a["label"] = label
 local function open_in_current_tab(input_path)
-  return open_root("edit", derive_root((_2aconfig_2a).roots, input_path))
+  return open_root("edit", derive_root(input_path))
 end
 _2amodule_2a["open_in_current_tab"] = open_in_current_tab
 local function open_in_new_tab(input_path)
-  return open_root("tabedit", derive_root((_2aconfig_2a).roots, input_path))
+  return open_root("tabedit", derive_root(input_path))
 end
 _2amodule_2a["open_in_new_tab"] = open_in_new_tab
-local function setup(_2_)
-  local _arg_3_ = _2_
-  local roots = _arg_3_["roots"]
-  local telescope_options = _arg_3_["telescope_options"]
-  _2aconfig_2a["roots"] = roots
-  _2aconfig_2a["telescope_options"] = telescope_options
-  return nil
+local function setup(opts)
+  return a["merge!"](_2aconfig_2a, opts)
 end
 _2amodule_2a["setup"] = setup
---[[ (local zsh_path "/Users/fuad/.config/zsh") (derive_root (. *config* "roots") zsh_path) (setup {:roots [{:path "~/Source/fuadsaud/Monrovia"}
+--[[ (setup {:roots [{:path "~/Source/fuadsaud/Monrovia"}
          {:path "~/Source/fuadsaud/fuadsaud.github.io"}
          {:path "~/.ditmas"}
          {:path "~/.config/nvim"}
-         {:entrypoint ".zshrc" :path "~/.config/zsh"}]}) (open_in_new_tab zsh_path) ]]
+         {:entrypoint ".zshrc" :path "~/.config/zsh"}]}) (. *config* "roots") (local zsh_path "/Users/fuad/.config/zsh") (local zsh_root (derive_root zsh_path)) (label* zsh_path) (open_in_new_tab zsh_path) ]]
 return _2amodule_2a
